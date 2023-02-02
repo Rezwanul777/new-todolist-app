030c78f8cccfeb962611cc825154de94d419bd98
Rezwanu Haque Prince <rezwanul.haquebd777@gmail.com>
2023-02-01 20:51:04 -0800

    update data complete

--- src/todolist/Todolists.js
+++ src/todolist/Todolists.js
@@ -1,25 +1,56 @@
 import React, { useState } from 'react'
 import './todolist.css'
 import todo from './image/todo.jpg'
-import { FaPlus, FaTrash } from "react-icons/fa";
+import { FaEdit, FaPlus, FaTrash } from "react-icons/fa";
 
 const Todolists = () => {
+
    const [addItems,setAddItems]=useState('')
    const [items,setItems]=useState([])
+   const [toggleSubmit,setToggleSubmit]=useState(true)
+   const[isEditItem,setIsEditItem]=useState(null)
 
    const addList=()=>{
     if(!addItems){
       alert("Please add item")
-    }else{
-      setItems([...items,addItems]) 
+    }else if(addItems && !toggleSubmit){
+      setItems(
+         
+            items.map((item)=>{
+               if(item.id===isEditItem){
+                  return{ ...item, name:addItems}
+               }
+               return item;
+            })
+         
+      )
+      setToggleSubmit(true)
+      setAddItems("")
+      setIsEditItem(null)
+    }
+    
+    else{
+      const allAddItems={id:new Date().getTime().toString(),name:addItems}
+      setItems([...items,allAddItems]) 
       console.log([addItems])
       setAddItems('')
     }
    }
+
+   //update item
+   const editItem=(id)=>{
+      const updateItems=items.find((item)=>{
+         return id===item.id
+      })
+     console.log(updateItems);
+      setToggleSubmit(false)
+      setAddItems(updateItems.name)
+      setIsEditItem(id)
+   }
       // delete Item
-   const deleteItem=(id)=>{
-      const selectedItem=items.filter((item,index)=>{
-      return index!==id
+   const deleteItem=(index)=>{
+      const selectedItem=items.filter((item)=>{
+      return index!==item.id
 
       })
       setItems(selectedItem);
@@ -43,18 +74,24 @@ const Todolists = () => {
          </figure>
          <div className='addItems'>
                <input type="text" placeholder='add items' value={addItems}
-                onChange={(e)=>setAddItems(e.target.value) } 
-             />
-               <FaPlus className='fa fa-plus' title="add item" onClick={addList}/>
+                onChange={(e)=>setAddItems(e.target.value) } />
+                {
+                   toggleSubmit ? <FaPlus className='fa fa-plus' title="add item" onClick={addList}/>
+                  :<FaEdit className='fa-edit' title='edit-item' onClick={addList}/>
+                }
 
+             
          </div>
             <div className='showItem'>
               {
-               items.map((item,index)=>{
+               items.map((item)=>{
                   return(
-                     <div className="eachItem" key={index}>
-                     <h3>{item}</h3>
-                     <FaTrash className='fa-trash' onClick={()=>deleteItem(index)}/>
+                     <div className="eachItem" key={item.id}>
+                     <h3>{item.name}</h3>
+                     <div className='todo-btn'>
+                       <FaEdit className='fa-edit' title='edit-item' onClick={()=>editItem(item.id)}/>
+                        <FaTrash className='fa-trash'title='delete-item' onClick={()=>deleteItem(item.id)}/>
+                     </div>
                   </div>
                   )
                })
