95d34aa8979bfe298663dc941ba5132b674ecabb
Rezwanu Haque Prince <rezwanul.haquebd777@gmail.com>
2023-02-01 16:22:23 -0800

    input field add item

--- src/todolist/Todolists.js
+++ src/todolist/Todolists.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './todolist.css'
 import todo from './image/todo.jpg'
-import { FaPlus } from "react-icons/fa";
+import { FaPlus, FaTrash } from "react-icons/fa";
 
 const Todolists = () => {
+   const [addItems,setAddItems]=useState('')
+   const [items,setItems]=useState([])
+
+   const addList=()=>{
+    if(!addItems){
+      alert("Please add item")
+    }else{
+      setItems([...items,addItems]) 
+      console.log([addItems])
+      setAddItems('')
+    }
+   }
+
   return (
    <>
    <div className='main-div'>
@@ -15,11 +28,27 @@ const Todolists = () => {
             </figcaption>
          </figure>
          <div className='addItems'>
-               <input type="text" placeholder='add items' />
-               <FaPlus className='fa fa-plus' title="add item"
-               />
+               <input type="text" placeholder='add items' value={addItems}
+                onChange={(e)=>setAddItems(e.target.value) } 
+             />
+               <FaPlus className='fa fa-plus' title="add item" onClick={addList}/>
 
          </div>
+            <div className='showItem'>
+              {
+               items.map((item,index)=>{
+                  return(
+                     <div className="eachItem" key={index}>
+                     <h3>{item}</h3>
+                     <FaTrash className='fa-trash'/>
+                  </div>
+                  )
+               })
+              }
+            </div>
+            <div className='showItem'>
+               <button className='btn'>CheckList</button>
+            </div>
       </div>
    </div>
    </>
